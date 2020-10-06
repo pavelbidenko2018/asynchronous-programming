d0c7bb8b2fd515a10acc2a7fdacf854a783c2441
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-10-06 23:54:43 +0200

    08-rest-fetch ex1

--- isolate/08-async-await-fetch-rest/exercises/exercise-1.js
+++ isolate/08-async-await-fetch-rest/exercises/exercise-1.js
@@ -6,53 +6,53 @@ const expect = chai.expect;
 
 
 
-const fetchPhotoWithTitle = async (title = '') => {
-  const titleParam = 'title=' + title;
-  const encodedTitleParam = _(titleParam);
-  const URL = _;
-  log(URL);
+const fetchPhotoWithTitle = async(title = '') => {
+    const titleParam = 'title=' + title;
+    const encodedTitleParam = _(titleParam);
+    const URL = `https://jsonplaceholder.typicode.com/${titleParam}`;
+    log(URL);
 
-  const response = await fetch(URL);
-  if (_) {
-    _;
-  }
+    const response = await fetch(URL);
+    if (!response.ok == null || response.status !== 200) {
+        throw new TypeError('response is not ok');
+    }
 
-  const data = await response.json();
-  const firstMatchingEntry = data[0];
-  return firstMatchingEntry;
+    const data = await response.json();
+    const firstMatchingEntry = data[0];
+    return firstMatchingEntry;
 
 };
 
 
 const testPhoto = (title) => {
-  return (photo) => {
-    log("title:", title);
-    it(`Photo should have title "${title}"`, () => {
-      expect(photo.title).to.equal(title);
-    });
-    console.log('');
-  }
+    return (photo) => {
+        log("title:", title);
+        it(`Photo should have title "${title}"`, () => {
+            expect(photo.title).to.equal(title);
+        });
+        console.log('');
+    }
 };
 
 const title1 = 'error magni fugiat dolorem impedit molestiae illo ullam debitis';
 const testPhoto1 = testPhoto(title1);
 fetchPhotoWithTitle(title1)
-  .then(photo => testPhoto1(photo))
-  .catch(err => testPhoto1(err));
+    .then(photo => testPhoto1(photo))
+    .catch(err => testPhoto1(err));
 
 
 const title2 = 'dignissimos et quo omnis';
 const testPhoto2 = testPhoto(title2);
 fetchPhotoWithTitle(title2)
-  .then(photo => testPhoto2(photo))
-  .catch(err => testPhoto2(err));
+    .then(photo => testPhoto2(photo))
+    .catch(err => testPhoto2(err));
 
 
 const title3 = 'possimus a et soluta';
 const testPhoto3 = testPhoto(title3);
 fetchPhotoWithTitle(title3)
-  .then(photo => testPhoto3(photo))
-  .catch(err => testPhoto3(err));
+    .then(photo => testPhoto3(photo))
+    .catch(err => testPhoto3(err));
 
 
 
